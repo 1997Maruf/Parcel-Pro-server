13b208f82013d2470bac569a42a32477720b5d05
maruf hossen <mdmdrufmeze@gmail.com>
2024-06-10 06:22:34 -0700

    install jsonwebtoken

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require("express");
+var jwt = require('jsonwebtoken');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a87xhva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,76 +26,77 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const bookingCollection = client
-      .db("parcel")
-      .collection("booking");
-    const userCollection = client
-      .db("parcel")
-      .collection("users");
+    const bookingCollection = client.db("parcel").collection("booking");
+    const userCollection = client.db("parcel").collection("users");
+    //jwt related api
+    app.post('/jwt', async(req, res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'});
+        res.send({token})
+    })
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.find(user).toArray();
+      res.send(result);
+    });
+    // update admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    // update Delivery
+    app.patch("/users/delivery/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "delivery",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-   app.post('/booking', async(req, res) =>{
-    const booking = req.body;
-    console.log(booking);
-    const result = await bookingCollection.insertOne(booking);
-    res.send(result);
-   })
-   app.post('/users', async(req, res) =>{
-    const  user =req.body;
-   const query = {email: user.email}
-   const existingUser= await userCollection.findOne(query);
-   if(existingUser){
-    return res.send({message: 'user already exists', insertedId: null});
-   }
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-   })
-   app.get('/users', async(req, res) =>{
-    const  user =req.body;
-    const result = await userCollection.find(user).toArray();
-    res.send(result);
-   })
-// update admin
-   app.patch('/users/admin/:id', async(req, res)=>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)};
-    const updatedDoc = {
-        $set:{
-            role:'admin'
-        }
-    }
-    const result = await userCollection.updateOne(filter, updatedDoc);
-    res.send(result);
-   })
-// update Delivery
-   app.patch('/users/delivery/:id', async(req, res)=>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)};
-    const updatedDoc = {
-        $set:{
-            role:'delivery'
-        }
-    }
-    const result = await userCollection.updateOne(filter, updatedDoc);
-    res.send(result);
-   })
-
-   app.get('/booking', async(req, res) =>{
-    console.log(req.query.email);
-    console.log(req.query);
-    let query= {};
-    if(req.query?.email) {
-        query ={email: req.query.email}
-    }
-    console.log(query);
-    const result = await bookingCollection.find(query).toArray();
-    res.send(result);
-   })
-
-
+    app.get("/booking", async (req, res) => {
+      console.log(req.query.email);
+      console.log(req.query);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      console.log(query);
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -107,12 +104,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("parcel is running");
+});
 
-
-app.get('/', (req, res) =>{
-    res.send('parcel is running')
-})
-
-app.listen(port, () =>{
-    console.log(`parcel server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`parcel server is running on port ${port}`);
+});
